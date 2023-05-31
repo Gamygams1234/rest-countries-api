269d980daa31a9e8196889ba7e6ceb0724ef49e8
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2023-05-30 17:46:06 -0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React, {  useContext } from "react";
+import clsx from 'clsx';
+import { ThemeContext } from "./ThemeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon as faMoonRegular } from "@fortawesome/free-regular-svg-icons";
+
+
+export default function Header(props) {
+  const context = useContext(ThemeContext);
+  const element = <FontAwesomeIcon icon={faMoon} />;
+  const elementRegular = <FontAwesomeIcon icon={faMoonRegular} />;
+
+  const classes = clsx(
+    {
+      dark: context.theme === "dark",
+    },
+    "header",
+    "outside-container"
+  );
+
+  return (
+    <div className={classes}>
+      <div className="inner-container">
+        <div className="logo fw-800">Where in the world?</div>
+
+        <div className="darkmode-button fw-300" onClick = {context.toggleTheme}>
+          {context.theme === "dark" ? element : elementRegular} Dark Mode
+        </div>
+      </div>
+    </div>
+  );
+}
